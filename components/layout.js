0ce21c7652446fea6145692cbca9836dd8cb6d59
mooxiu <monetshaw@outlook.com>
2023-01-07 18:17:55 +0900

    change name

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Muyao Xiao';
+const name = 'MOOXIU';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
